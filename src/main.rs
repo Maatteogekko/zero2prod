253bef0d0555160070890d61ef7b96b5371c1f05
Maatteogekko <matteogent9991@gmail.com>
2023-04-27 00:10:18 +0200

    add health-check and remove greet

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use std::io;
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-
-    format!("Hello {name}!")
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
