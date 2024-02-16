275be377f400662d57109807ed5b17443608eddb
Maatteogekko <matteogent9991@gmail.com>
2024-02-16 22:14:46 +0100

    subscriptions endpoint definition

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,27 @@ use std::{io, net::TcpListener};
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 pub fn run(listener: TcpListener) -> io::Result<Server> {
-    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
 
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
